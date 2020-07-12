f00d7a385616f9162161afbefdf6f9faa3e5a13d
Takanori Sato <taka_sato@yumemi.co.jp>
2020-07-12 17:22:28 +0900

    familiesサンプル作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// グローバル変数でProviderを宣言
-final firstNameProvider = StateProvider((ref) => 'Mona');
-final lastNameProvider = StateProvider((ref) => 'Lisa');
+const japan = 'Japan';
+const america = 'America';
 
-// Computedを使ってイニシャルを返すproviderを宣言
-// 例えばfirstNameがMariaに変更され、firstNameが変わるがinitialsが変わらない場合、
-// Computedを使うことでinitialsを使うWidgetはrebuildされない(buildの最適化)
-final Computed initialsProvider = Computed((read) {
-  final firstName = read(firstNameProvider).state;
-  final lastName = read(lastNameProvider).state;
-
-  return '${firstName[0]}${lastName[0]}';
+// familyを使ったprovider
+// Providerにvalueを持たせられる
+final titleProvider = Provider.family<String, String>((ref, country) {
+  if (country == japan) {
+    return 'こんにちは、日本';
+  } else if (country == america) {
+    return 'Hello America';
+  } else {
+    throw UnsupportedError('Unknown country $country');
+  }
 });
+
+final countryProvider = StateProvider((ref) => japan);
 
 void main() {
   runApp(
@@ -38,14 +41,15 @@ class MyApp extends StatelessWidget {
           // Providerを使うにはConsumerを使う
           child: Consumer(
             (context, read) {
-              final initials = read(initialsProvider);
-              return Text(initials);
+              // 通常のProviderとは違い引数に値を入れる必要がある
+              final country = read(countryProvider);
+              final title = read(titleProvider(country.state));
+              return Text(title);
             },
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          // initialsの状態は変更ないため、ConsumerのTextはrebuildされない
-          onPressed: () => firstNameProvider.read(context).state = 'Maria',
+          onPressed: () => countryProvider.read(context).state = america,
           child: const Icon(Icons.check),
         ),
       ),
